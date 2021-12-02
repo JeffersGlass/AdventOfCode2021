a7cf381069ff983f59faefed6f27995c978163ba
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-01 19:22:45 -0600

    Remove unused line

--- day1/part1.py
+++ day1/part1.py
@@ -1,8 +1,6 @@
 with open("input.txt", "r") as infile:
     data = [int(t) for t in infile.read().split('\n')]
 
-pairs = zip(data[:-1], data[1:])
-
 numDecreases = len([pair for pair in zip(data[:-1], data[1:]) if pair[0] < pair[1]])
 
 print(f"{numDecreases=}")
