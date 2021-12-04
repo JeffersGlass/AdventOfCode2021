9fa9e9880676c5c0eafaeec7b94ab36ae7c2fc4c
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-04 12:59:10 -0600

    Clean up solution to day4, part 1

--- day4/part1.py
+++ day4/part1.py
@@ -1,6 +1,7 @@
 with open ("input.txt", 'r') as infile:
     inputChunks = infile.read().split('\n\n')
 
+#---Parse the Input to make it useful----
 
 allCalledNumbers = [int(num) for num in inputChunks[0].split(',')]
 boards = [' '.join(chunk.split('\n')) for chunk in inputChunks[1:]] #get boards
@@ -16,6 +17,8 @@ boardRows = [[b[index:index+5] for index in range(0, 25, 5)] for b in intBoards]
 #each of which is a list of ints
 boardCols = [[[b[row][index] for row in range(5)] for index in range(5)] for b in boardRows]
 
+#---Define some functions to help us solve the problem as written---
+
 #For a given board (by row or column), are any of its lines made up only of numbers in 'calledNums'?
 def isBoardAWin(board, calledNums):
     return any([all([num in calledNums for num in line]) for line in board])
@@ -25,31 +28,25 @@ def calcScore(board, calledNumbers):
     unusedNumbers = [num for line in board for num in line if num not in calledNumbers]
     return sum(unusedNumbers) * calledNumbers[-1]
 
+def doWin(board, calledNumbers):
+    print(f"Score is {calcScore(b, calledSoFar)}")
+    exit()
+
+#---Find solution---
+
 for i in range(len(allCalledNumbers)):
     calledSoFar = allCalledNumbers[:i]
     print(f"Searching for wins with numbers {calledSoFar}")
+    winner = False
     for b in boardRows:
         if isBoardAWin(b, calledSoFar):
-            print(f"Winner board by row!")
-            print(b)
-            print(f"Score is {calcScore(b, calledSoFar)}")
-            exit()
+            print(f"Winner board by row! \n {b}")
+            doWin(b, calledSoFar)
+
     
     for b in boardCols:
         if isBoardAWin(b, calledSoFar):
-            print(f"Winner board by column!!")
-            print(b)
-            print(f"Score is {calcScore(b, calledSoFar)}")
-            exit()
-    
-    print(f"No winners after {i} numbers, calling next number")
-    
-""" print("By Rows:")
-for i in range(len(boards)):
-    print(boardRows[i], end = "")
-    print(f"Win?: {isBoardAWin(boardRows[i], calledNumbers)}")
+            print(f"Winner board by column! \n {b}")
+            doWin(b, calledSoFar)
 
-print("By Rows:")
-for i in range(len(boards)):
-    print(boardRows[i], end = "")
-    print(f"Win?: {isBoardAWin(boardCols[i], calledNumbers)}") """
+    print(f"No winners after {i} numbers, calling next number")
