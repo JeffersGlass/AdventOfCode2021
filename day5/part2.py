f8f55d2a46d4887d3315b44e13e78f1f4a9676e5
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-05 12:56:09 -0600

    Reformat part 2

--- day5/part2.py
+++ day5/part2.py
@@ -12,57 +12,37 @@ for d in data:
 
 horizontalLines = [l for l in parsedData if l.p1y == l.p2y]
 verticalLines = [l for l in parsedData if l.p1x == l.p2x]
-diagonalLines = [l for l in parsedData if l not in verticalLines and l not in horizontalLines]
 
 coveredOnce, coveredMany = set(), set()
 
-#Loop over all the points in the lines we care about
 for l in horizontalLines:
-    #print(f"Processing horizontal line from ({l.p1x},{l.p1y}) to ({l.p2x},{l.p2y})")
-    #print(f"\tProcessing Points")
     for xCoord in range(min(l.p1x, l.p2x), max(l.p1x, l.p2x)+1):
         point = (xCoord, l.p1y)
-        #print(f"\t\t{point} ", end = "")
         #If the point is already covered by multiple lines, we don't need to do anything more with it, but
         if point not in coveredMany:
             #If we haven't see it at all, we should mark that we've now seen it once.
             if point not in coveredOnce: 
                 coveredOnce.add(point)
-                #print(" has now been seen once")
             #Otherwise, we should mark that we've now seen it in multiple lines
             else:
                 coveredOnce.remove(point)
                 coveredMany.add(point)
-                #print(" was seen once before, has now been seen multiple times")
-        else:
-            pass
-            #print("Has already been seen multiple times")
-    #print(f"\t{len(coveredMany)} points are covered at least twice")
 
-#Loop over all the points in the lines we care about
 for l in verticalLines:
-    #print(f"Processing vertical line from ({l.p1x},{l.p1y}) to ({l.p2x},{l.p2y})")
-    #print(f"\tProcessing Points")
     for yCoord in range(min(l.p1y, l.p2y), max(l.p1y, l.p2y)+1):
         point = (l.p1x, yCoord)
-        #print(f"\t\t{point} ", end = "")
         #If the point is already covered by multiple lines, we don't need to do anything more with it, but
         if point not in coveredMany:
             #If we haven't see it at all, we should mark that we've now seen it once.
             if point not in coveredOnce: 
                 coveredOnce.add(point)
-                #print(" has now been seen once")
             #Otherwise, we should mark that we've now seen it in multiple lines
             else:
                 coveredOnce.remove(point)
                 coveredMany.add(point)
-                #print(" was seen once before, has now been seen multiple times")
-        else:
-            pass
-            #print("Has already been seen multiple times")
-    #print(f"\t{len(coveredMany)} points are covered at least twice")
 
-#loop over all the diagonal lines:
+diagonalLines = [l for l in parsedData if l not in verticalLines and l not in horizontalLines]
+
 for l in diagonalLines:
     #Make sure the first coordinate of each line is left of the second coordinte
     if l.p2x < l.p1x: l = Line(p1x = l.p2x, p1y = l.p2y, p2x = l.p1x, p2y = l.p1y)
@@ -76,14 +56,9 @@ for l in diagonalLines:
             #If we haven't see it at all, we should mark that we've now seen it once.
             if point not in coveredOnce: 
                 coveredOnce.add(point)
-                #print(" has now been seen once")
             #Otherwise, we should mark that we've now seen it in multiple lines
             else:
                 coveredOnce.remove(point)
                 coveredMany.add(point)
-                #print(" was seen once before, has now been seen multiple times")
-        else:
-            pass
-            #print("Has already been seen multiple times")
 
 print(f"Solution is: {len(coveredMany)}")
