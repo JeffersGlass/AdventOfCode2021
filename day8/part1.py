c71ca8cd1aff3df3288ec1a7780196c0293771fe
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-08 07:55:16 -0600

    Cleanup part 1 solution

--- day8/part1.py
+++ day8/part1.py
@@ -8,9 +8,5 @@ class displayRun:
 with open("input.txt", "r", encoding="utf-8") as infile:
     runs = [displayRun(hookups = line.split('|')[0].strip().split(" "), outputs = line.split('|')[1].strip().split(" ")) for line in infile.read().split('\n')]
 
-sum = 0
-for r in runs:
-    for o in r.outputs:
-        if len(o) in [2, 3, 4, 7]: sum+=1
-
-print(f"{sum=}")
+totalUnique = sum([sum([(1 if len(segments) in [2,3,4,7] else 0) for segments in r.outputs]) for r in runs])
+print(f"{totalUnique=}")
