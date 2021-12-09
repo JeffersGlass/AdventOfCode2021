7e2ae145f38408c8a6e1d06ee3c8ea72fe85f4bb
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-09 07:22:49 -0600

    Solution to part 1

---
+++ day9/part1.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+data = defaultdict(lambda:100)
+
+with open("input.txt", "r", encoding="utf-8") as infile:
+    for r, row in enumerate(infile.read().split('\n')):
+        numRows = r + 1
+        for c, col in enumerate(row):
+            numCols = c + 1
+            data[(r, c)] = int(col)
+
+total = 0
+neighborDirections = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+for r in range(numRows):
+    for c in range(numCols):
+        #print([(r + deltar, c + deltac) for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac)])
+        if all([data[(r, c)] < data[(r + deltar, c + deltac)] for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac)]):
+            print(f"Low Point Found: ({r},{c}) : {data[(r, c)]}")
+            total += data[(r, c)] + 1
+        else:
+            print(f"Not: ({r},{c}) : {data[(r, c)]}", end = " ")
+            print(f" {[data[(r + deltar, c + deltac)] for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac)]}")
+
+
+print(f"{total= }")
