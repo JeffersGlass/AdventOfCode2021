edb2a221775eb92acfb604546b7d45cc436b273a
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-09 07:45:56 -0600

    Easier data size calcualtion

--- day9/part2.py
+++ day9/part2.py
@@ -4,10 +4,11 @@ from math import prod
 data = defaultdict(lambda:9)
 
 with open("input.txt", "r", encoding="utf-8") as infile:
-    for r, row in enumerate(infile.read().split('\n')):
-        numRows = r + 1
+    raw = infile.read().split('\n')
+    numRows = len(raw)
+    numCols = len(raw[0])
+    for r, row in enumerate(raw):
         for c, col in enumerate(row):
-            numCols = c + 1
             data[(r, c)] = int(col)
 
 basins = list()
@@ -18,14 +19,11 @@ for r in range(numRows):
         if all([data[(r, c)] < data[(r + deltar, c + deltac)] for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac)]):
             basins.append([(r, c),])
 
-#Flood fill each basin until it hits a '9'
+#Flood fill each basin until it hits a wall 
 basinLengths = []
 for b in basins:
     for pos in b:
-        b.extend([(pos[0] + deltar, pos[1] + deltac) for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] \
-            if (deltar*deltac == 0 and deltar != deltac and \
-                data[(pos[0] + deltar, pos[1] + deltac)] != 9 and \
-                (pos[0] + deltar, pos[1] + deltac) not in b)])
+        b.extend([(pos[0] + deltar, pos[1] + deltac) for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac and data[(pos[0] + deltar, pos[1] + deltac)] != 9 and (pos[0] + deltar, pos[1] + deltac) not in b)])
     basinLengths.append(len(b))
 
 print(f"Solution: {prod(sorted(basinLengths, reverse=True)[:3])}")
