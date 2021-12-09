dd9b95fae6c57afbd854b619727c7e08962b07be
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-09 07:39:54 -0600

    Comments for part 2

--- day9/part2.py
+++ day9/part2.py
@@ -10,14 +10,15 @@ with open("input.txt", "r", encoding="utf-8") as infile:
             numCols = c + 1
             data[(r, c)] = int(col)
 
-total = 0
 basins = list()
 
+#Find the low point for each bsain (as in part 1)
 for r in range(numRows):
     for c in range(numCols):
         if all([data[(r, c)] < data[(r + deltar, c + deltac)] for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac)]):
             basins.append([(r, c),])
 
+#Flood fill each basin until it hits a '9'
 basinLengths = []
 for b in basins:
     for pos in b:
