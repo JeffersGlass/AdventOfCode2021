71cfea88b74f75db51b78fbd31e7da28322ebe07
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-09 07:23:20 -0600

    Cleanup part 1

--- day9/part1.py
+++ day9/part1.py
@@ -16,11 +16,7 @@ for r in range(numRows):
     for c in range(numCols):
         #print([(r + deltar, c + deltac) for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac)])
         if all([data[(r, c)] < data[(r + deltar, c + deltac)] for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac)]):
-            print(f"Low Point Found: ({r},{c}) : {data[(r, c)]}")
             total += data[(r, c)] + 1
-        else:
-            print(f"Not: ({r},{c}) : {data[(r, c)]}", end = " ")
-            print(f" {[data[(r + deltar, c + deltac)] for deltar in [-1, 0, 1] for deltac in [-1, 0, 1] if (deltar*deltac == 0 and deltar != deltac)]}")
 
 
 print(f"{total= }")
