6aca89228abad59256c1afb2eadf078a880cdf44
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-10 07:20:37 -0600

    Solution to day 10 part 1

---
+++ day10/part1.py
@@ -0,0 +1,24 @@
+with open ("input.txt", "r", encoding='utf-8') as infile:
+    data = infile.read().split("\n")
+
+lefts  = [ '(',     '[',    '{',    '<' ]
+rights = [ ')',     ']',    '}',    '>' ]
+points = [ 3,       57,     1197,   25137]
+
+totalScore = 0
+
+for line in data:
+    stack = list()
+    for char in line:
+        if char in lefts:
+            stack.append(char)
+        elif char in rights:
+            lastLeft = stack.pop()
+            if lefts.index(lastLeft) != rights.index(char):
+                totalScore += points[rights.index(char)]
+        else: raise ValueError(f"Unkown character {char}")
+    else:
+        continue
+    break
+
+print(f"{totalScore= }")
