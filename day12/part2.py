fa7031a61b31c375d1aa2403471134aac64a3447
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-12 21:54:12 -0600

    Progress, need to treat start differently

--- day12/part2.py
+++ day12/part2.py
@@ -1,45 +1,62 @@
 from dataclasses import dataclass
 
-with open("input.txt", "r", encoding="utf-8") as infile:
+with open("input_test.txt", "r", encoding="utf-8") as infile:
     data = [line.split('-') for line in infile.read().split('\n')]
 
 @dataclass
 class Route():
     pathSoFar: list
     currentRoom: str
-    littleRoomsVisited: list # of tuples: ['a', numberOfTimesVisited]
+    littleRoomsVisited: dict #['a', numberOfTimesVisited]. This is the key change
 
-startingPaths = [path for path in data if 'start' in path]
+startingPaths = [path for path in data if 'start' in path] 
+routesInProgress = list()
+
+for p in startingPaths:
+    route = p.copy()
+    s = p.copy()
+    s.remove('start')
+    current = s[0]
+    if s[0] == s[0].lower(): littles = {'start':2, str(s[0]):1}
+    else: littles = {'start':2}
+    routesInProgress.append(Route(pathSoFar=[route], littleRoomsVisited = littles, currentRoom = current))
 
 #TODO Treat 'start' differently, adjust little-rooms-visited init
-routesInProgress = [Route(pathSoFar=[s], littleRoomsVisited= ['start'] if not(all([item == item.lower() for item in s])) else s, currentRoom = list(set(s) - {'start'})[0]) for s in startingPaths]
 completeRoutes = list()
 
 while(len(routesInProgress) > 0):
     nextRoutes = list()
+    print("")
+    print(f"ProcessingRoutes: {len(routesInProgress)}")
+    print(f"CompletedRoutes:  {len(completeRoutes)}")
 
     for r in routesInProgress:
+        print(f"Examining route {r}", end = "")
         #Check if we're reached the end
         if r.currentRoom == 'end':
+            print(", which is complete!")
             completeRoutes.append(r)
         else:
+            print("")
             nextPaths = [path for path in data if r.currentRoom in path]
             for p in nextPaths:
+                print(f"\tPossible next path: {p}", end = "")
                 nextRoom = [item for item in p if item != r.currentRoom][0]
-                #TODO Treat littleRoomsViisted differently based on new rules for p2
-                if nextRoom not in r.littleRoomsVisited:
+                print(f" leading to room {nextRoom}")
+                #TODO Treat littleRoomsVisted differently based on new rules for p2
+                if nextRoom not in r.littleRoomsVisited or (r.littleRoomsVisited[nextRoom] == 1 and not any([(v >= 2) for v in r.littleRoomsVisited.values()])):
                     newPath = r.pathSoFar + [p]
+                    newLittleRooms = r.littleRoomsVisited.copy()
                     if nextRoom == nextRoom.lower():
-                        #TODO Treat littleRoomsViisted differently based on new rules for p2
-                        newLittleRooms = r.littleRoomsVisited + [nextRoom]
-                    else:
-                        newLittleRooms = r.littleRoomsVisited
-                    #TODO Treat littleRoomsViisted differently based on new rules for p2
+                        if nextRoom in r.littleRoomsVisited:
+                            newLittleRooms[nextRoom] += 1
+                        else:
+                            newLittleRooms[nextRoom] = 1
                     nextRoutes.append(Route(pathSoFar = newPath, littleRoomsVisited = newLittleRooms, currentRoom = nextRoom))
                 else:
                     pass
 
     routesInProgress = nextRoutes
-    #input("Press enter")
+    input("Press enter")
 
 print(f"Total routes: {len(completeRoutes)}")
