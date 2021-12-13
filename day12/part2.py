53bd86a6b4492b676f02ba9a1a8da85d8f9f4f74
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-12 21:21:58 -0600

    Add todos to shape solution to p2

--- day12/part2.py
+++ day12/part2.py
@@ -10,6 +10,8 @@ class Route():
     littleRoomsVisited: list # of tuples: ['a', numberOfTimesVisited]
 
 startingPaths = [path for path in data if 'start' in path]
+
+#TODO Treat 'start' differently, adjust little-rooms-visited init
 routesInProgress = [Route(pathSoFar=[s], littleRoomsVisited= ['start'] if not(all([item == item.lower() for item in s])) else s, currentRoom = list(set(s) - {'start'})[0]) for s in startingPaths]
 completeRoutes = list()
 
@@ -24,12 +26,15 @@ while(len(routesInProgress) > 0):
             nextPaths = [path for path in data if r.currentRoom in path]
             for p in nextPaths:
                 nextRoom = [item for item in p if item != r.currentRoom][0]
+                #TODO Treat littleRoomsViisted differently based on new rules for p2
                 if nextRoom not in r.littleRoomsVisited:
                     newPath = r.pathSoFar + [p]
                     if nextRoom == nextRoom.lower():
+                        #TODO Treat littleRoomsViisted differently based on new rules for p2
                         newLittleRooms = r.littleRoomsVisited + [nextRoom]
                     else:
                         newLittleRooms = r.littleRoomsVisited
+                    #TODO Treat littleRoomsViisted differently based on new rules for p2
                     nextRoutes.append(Route(pathSoFar = newPath, littleRoomsVisited = newLittleRooms, currentRoom = nextRoom))
                 else:
                     pass
