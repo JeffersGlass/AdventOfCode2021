d79252d506021939c753308395d53d7ce043329a
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-12 21:18:15 -0600

    Cleanup solution

--- day12/part1.py
+++ day12/part1.py
@@ -15,22 +15,15 @@ completeRoutes = list()
 
 while(len(routesInProgress) > 0):
     nextRoutes = list()
-    print("")
-    print(f"ProcessingRoutes: {len(routesInProgress)}")
-    print(f"CompletedRoutes:  {len(completeRoutes)}")
 
     for r in routesInProgress:
         #Check if we're reached the end
         if r.currentRoom == 'end':
             completeRoutes.append(r)
-            print(f"Route completed {r}")
         else:
-            print(f"Examining route {r}")
             nextPaths = [path for path in data if r.currentRoom in path]
             for p in nextPaths:
-                print(f"\tPossible next path: {p}", end = "")
                 nextRoom = [item for item in p if item != r.currentRoom][0]
-                print(f", next room would be {nextRoom}", end = "")
                 if nextRoom not in r.littleRoomsVisited:
                     newPath = r.pathSoFar + [p]
                     if nextRoom == nextRoom.lower():
@@ -38,15 +31,10 @@ while(len(routesInProgress) > 0):
                     else:
                         newLittleRooms = r.littleRoomsVisited
                     nextRoutes.append(Route(pathSoFar = newPath, littleRoomsVisited = newLittleRooms, currentRoom = nextRoom))
-                    print(", so we'll check that next time")
                 else:
-                    print(", but already been to that little room")
+                    pass
 
     routesInProgress = nextRoutes
     #input("Press enter")
 
-print("_______")
-print("COMPLETE ROUTES")
-for r in completeRoutes:
-    print(r)
-print(len(completeRoutes))
+print(f"Total routes: {len(completeRoutes)}")
