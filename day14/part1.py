525f4f5c657585efe85fcb6463064ad8da3cd977
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-14 08:04:27 -0600

    Solution to part 1

---
+++ day14/part1.py
@@ -0,0 +1,33 @@
+import re
+from collections import Counter
+from time import time
+
+with open("input.txt", "r", encoding="utf-8") as infile:
+    data = infile.read().split("\n\n")
+
+template = data[0]
+rules = dict()
+for line in data[1].split('\n'):
+    r = re.search(r'(..) -> (.)', line)
+    rules[r.group(1)] = r.group(2)
+
+def doStep(template):
+    additions = [" "] * len(template)
+    for i in range(len(template[:-1])):
+        letterPair = template[i:i+2]
+        if letterPair in rules: additions[i] = rules[letterPair]
+    
+    return ''.join([val for pair in zip(template, additions) for val in pair] + [template[-1]])
+
+startTime = time()
+
+for i in range(10):
+    template = doStep(template)
+
+#alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+counts = Counter(template)
+lCount = sorted(counts.items(), key=lambda x: x[1])
+
+minimum, maximum = lCount[0][1], lCount[-1][1]
+print(f"Result is {maximum - minimum= }")
