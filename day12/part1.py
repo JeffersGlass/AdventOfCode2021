0f863e8ab100a9c25d680b6865867d83d33b810d
Jeff Glass <glass.jeffrey@gmail.com>
2021-12-14 10:40:15 -0600

    cleanup

--- day12/part1.py
+++ day12/part1.py
@@ -6,35 +6,35 @@ with open("input.txt", "r", encoding="utf-8") as infile:
 @dataclass
 class Route():
     pathSoFar: list
-    currentRoom: str
-    littleRoomsVisited: list
+    currentCave: str
+    smallCavesVisited: list
 
 startingPaths = [path for path in data if 'start' in path]
-routesInProgress = [Route(pathSoFar=[s], littleRoomsVisited= ['start'] if not(all([item == item.lower() for item in s])) else s, currentRoom = list(set(s) - {'start'})[0]) for s in startingPaths]
+routesInProgress = [Route(pathSoFar=[s], smallCavesVisited= ['start'] if not(all([item == item.lower() for item in s])) else s, currentCave = list(set(s) - {'start'})[0]) for s in startingPaths]
 completeRoutes = list()
 
 while(len(routesInProgress) > 0):
     nextRoutes = list()
 
     for r in routesInProgress:
-        if r.currentRoom == 'end':
+        if r.currentCave == 'end':
             completeRoutes.append(r)
         else:
             #Get all path segments including hte current room
-            nextPaths = [path for path in data if r.currentRoom in path]
+            nextPaths = [path for path in data if r.currentCave in path]
             for p in nextPaths:
                 #Get the room this path segment would lead to (it's the room that's not our current room)
-                nextRoom = [item for item in p if item != r.currentRoom][0]
+                nextCave = [item for item in p if item != r.currentCave][0]
                 #Can't go to a little room twice
-                if nextRoom not in r.littleRoomsVisited:
+                if nextCave not in r.smallCavesVisited:
                     newPath = r.pathSoFar + [p]
-                    if nextRoom == nextRoom.lower():
+                    if nextCave == nextCave.lower():
                         #If we're now going to be in a little room, add it to the list of little rooms we've visited
-                        newLittleRooms = r.littleRoomsVisited + [nextRoom]
+                        newSmallCaves = r.smallCavesVisited + [nextCave]
                     else:
                         #Otherwise, the list of little rooms visited doens't change
-                        newLittleRooms = r.littleRoomsVisited
-                    nextRoutes.append(Route(pathSoFar = newPath, littleRoomsVisited = newLittleRooms, currentRoom = nextRoom))
+                        newSmallCaves = r.smallCavesVisited
+                    nextRoutes.append(Route(pathSoFar = newPath, smallCavesVisited = newSmallCaves, currentCave = nextCave))
                 else:
                     pass
 
